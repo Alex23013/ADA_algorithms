92619e1f38d8b9f6314a2719089957a95a6819a1
Maria Cornejo Lupa <alecornejolupa2013@gmail.com>
2017-09-26 23:05:59 -0500

    without bug xD

--- for_promedioFun.cpp
+++ for_promedioFun.cpp
@@ -14,9 +14,9 @@ for(int i = 0; i < 4; i++){
     for (int i = 0; i < tests ; i++) {
 
 		high_resolution_clock::time_point t1 = high_resolution_clock::now();
-				////*
-				codigo que quieras tomat tiempo xD
-				*////
+				///
+				codigo que quieras tomar tiempo xD
+				///
 
 		high_resolution_clock::time_point t2 = high_resolution_clock::now();
 		auto res = duration_cast<microseconds>( t2 - t1 ).count();
